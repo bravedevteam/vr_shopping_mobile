b7937f895fe0e62e39545d225fc89c2f0c99a0d4
TeamBrave_Dev <73458930+bravedevteam@users.noreply.github.com>
2021-03-30 15:12:23 +0900

    Add files via upload

---
+++ js/common.js
@@ -0,0 +1,51 @@
+$(function(){
+	$(".btnMap").click(function(){
+		$(this).toggleClass("active");
+	});
+
+	$(".btnSound").click(function(){
+		$(this).toggleClass("on");
+	});
+
+	$("#btnLNB").click(function(e){
+		e.preventDefault();
+		$("#boxLNB").addClass("on");
+	});
+
+	$("#boxLNB > a.close").click(function(e){
+		e.preventDefault();
+		$("#boxLNB").removeClass("on");
+	});
+
+	var mainBrand = new Swiper('.boxMainBrand .swiper-container', {
+		slidesPerView: 'auto',
+		spaceBetween: 20,
+		centeredSlides: true,
+		navigation: {
+			nextEl: '.boxMainBrand .swiper-button-next',
+			prevEl: '.boxMainBrand .swiper-button-prev',
+		},
+	});
+
+	$(".open--popup").click(function(){
+		var target = $(this).attr("data-pop");
+
+		$("#"+target).addClass("active");
+		$("#footer").addClass("is--fixed");
+		$("#dim").fadeIn(300);
+	});
+
+	$(".popBasic .slideClose").click(function(e){
+		e.preventDefault();
+
+		$(this).parent().removeClass("active");
+		$("#footer").removeClass("is--fixed");
+		$("#dim").fadeOut(300);
+	});
+
+	$("#dim").click(function(){
+		$(this).fadeOut(300);
+		$(".popBasic").removeClass("active");
+		$("#footer").removeClass("is--fixed");
+	});
+});
